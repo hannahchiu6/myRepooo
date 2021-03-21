b1d4c9517bf6bcf0b16bf83168d1c009f8367704
Hannah Chiu <77312212+hannahchiu6@users.noreply.github.com>
2021-03-21 21:51:08 +0800

    Update Closure

---
+++ Week4/hc_04.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let oddNumber: (Int) -> Bool = { number in
+
+  if number % 2 != 0 {
+    return true
+    } else {
+    return false
+    }
+ 
+}
+
+oddNumber(13)
+oddNumber(10)
+
+
+
+
+
